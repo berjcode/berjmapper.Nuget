a9ec97ddc41f30ad6a3f6867f8232737179b6847
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-05-04 16:52:37 +0300

    program.cs example added.

---
+++ ExampleProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using berjmapper;
+using ExampleProject;
+
+Console.WriteLine("Hello, World!");
+
+var Product = new Product
+{
+    Id = 1,
+    Name = "berjCode",
+    Description = "offers pride"
+};
+
+var ProductListDtoToProduct = new ProductListDto { Id = 2, Name = "Abdullah" };
+
+
+Console.WriteLine("----------- Converting Product to ProductListDto ------------");
+
+var berjMapper = new BerjMapper<Product, ProductListDto>();
+var convertedproductListDto = berjMapper.Map(Product);
+Console.WriteLine(convertedproductListDto.Id);
+Console.WriteLine(convertedproductListDto.Name);
+
+
+
+
+Console.WriteLine("----------- Converting ProductListDto to Product ------------");
+
+
+var convertedProduct = berjMapper.Map(ProductListDtoToProduct);
+
+
+Console.WriteLine(convertedProduct.Id);
+Console.WriteLine(convertedProduct.Name);
+
