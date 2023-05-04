0c8574c2f5ae5a32d5fc95fc3f078104a1a1d015
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-05-04 17:33:42 +0300

    Added Use of ListDto to the sample project.

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using berjmapper;
 using ExampleProject;
 
+
 Console.WriteLine("Hello, World!");
 
 var Product = new Product
@@ -10,25 +11,59 @@ var Product = new Product
     Description = "offers pride"
 };
 
-var ProductListDtoToProduct = new ProductListDto { Id = 2, Name = "Abdullah" };
+
+
+var ProductDtoToProduct = new ProductDto { Id = 2, Name = "Abdullah" };
 
 
 Console.WriteLine("----------- Converting Product to ProductListDto ------------");
 
-var berjMapper = new BerjMapper<Product, ProductListDto>();
-var convertedproductListDto = berjMapper.Map(Product);
-Console.WriteLine(convertedproductListDto.Id);
-Console.WriteLine(convertedproductListDto.Name);
+var berjMapper = new BerjMapper<Product, ProductDto>();
+var convertedproductDto = berjMapper.Map(Product);
+Console.WriteLine(convertedproductDto.Id);
+Console.WriteLine(convertedproductDto.Name);
 
 
 
 
-Console.WriteLine("----------- Converting ProductListDto to Product ------------");
+Console.WriteLine("----------- Converting ProductDto to Product ------------");
 
 
-var convertedProduct = berjMapper.Map(ProductListDtoToProduct);
+var convertedProduct = berjMapper.Map(ProductDtoToProduct);
 
 
 Console.WriteLine(convertedProduct.Id);
 Console.WriteLine(convertedProduct.Name);
 
+
+Console.WriteLine("----------- Test ------------");
+
+//var stopwatch = new Stopwatch();
+//stopwatch.Start();
+//for (int i = 0; i < 100000; i++)
+//{
+//    var source = new ProductDto();
+//    var destination = new berjMapper(Product,ProductDto>);
+//}
+////stopwatch.Stop();
+//Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+
+Console.WriteLine("----------- ProductListDto ------------");
+
+var products = new List<Product>()
+{
+    new Product { Id = 1, Name = "Product 1" },
+    new Product { Id = 2, Name = "Product 2"},
+    new Product { Id = 3, Name = "Product 3" },
+};
+
+var berjMapper2 = new BerjMapper<Product, ProductListDto>();
+//products => converting to ProductListDto.
+var convertedProductListDto = berjMapper2.Map(products);
+
+foreach(var productListDto in convertedProductListDto)
+{
+    Console.WriteLine(productListDto.Id);
+    Console.WriteLine(productListDto.Name);
+}
+ 
