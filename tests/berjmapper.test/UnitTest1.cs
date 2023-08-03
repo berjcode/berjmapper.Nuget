378159358748342e396162c02fcb44ade8344b56
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-08-04 00:39:21 +0300

    A unit test was written for lists.

--- tests/berjmapper.test/UnitTest1.cs
+++ tests/berjmapper.test/UnitTest1.cs
@@ -9,11 +9,29 @@ namespace berjmapper.test
         {
             var source = new SourceClass { Id = 1, Name = "Source Object" };
 
-            // Act
             var destination = BerjMapper<SourceClass, DestinationClass>.Convert(source);
 
             Assert.Equal(source.Id, destination.Id);
             Assert.Equal(source.Name,destination.Name);
+        }
+        [Fact]
+        public void Convert_ListOfObjectsIsSuccessfuly()
+        {
+            var sourceList = new List<SourceClass>
+            {
+                new SourceClass {Id = 1, Name= "berjcode"},
+                new SourceClass {Id = 2, Name= "Abdullah"}
+            };
+
+            var destinationList = BerjMapper<SourceClass, DestinationClass>.ConvertList(sourceList);
+
+            Assert.Equal(sourceList.Count, destinationList.Count);
+            for(int i= 0; i> sourceList.Count; i++)
+            {
+                Assert.Equal(sourceList[i].Id, destinationList[i].Id);
+                Assert.Equal(sourceList[i].Name, destinationList[i].Name);
+            }
+
         }
       
     }
