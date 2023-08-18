0beb951d32d350be3557ac2a0d77f2accfab227b
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-08-18 16:09:25 +0300

    A static structure was created for messages.

---
+++ berjmapper/Messagess/ExceptionMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace berjmapper.Messagess;
+
+public static class ExceptionMessage
+{
+    public static string ArgumentNullExceptionSourceMessage = "Source  Cannot Be Null";
+    public static string ArgumentNullExceptionnSourceListMessage = "SourceList Cannot Be Null";
+    public static string ArgumentNullExceptionDestinationMessage = "Destination  Cannot Be Null";
+    public static string ArgumentNullExceptionDestinationListMessage = "DestinationList Cannot Be Null";
+
+}
